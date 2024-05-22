004b8545045d09eb67f9bf83a62ef79c1d5386a7
Hansika <130871803+Hansika-y@users.noreply.github.com>
2024-05-22 16:13:42 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function bedroom()
+{
+    window.location="bedroom.html";
+}
+
+function tv_and_ac()
+{
+    window.location="tv_and_ac.html";
+}
+
+function desk()
+{
+    window.location="desk.html";
+}
+
+function bottles()
+{
+    window.location="bottles.html";
+}
+
+function fruit_basket()
+{
+    window.location="fruit_basket.html";
+}
